3b273dd2c226ae64e952359e16d22b9fb2e19ee6
Kaio Matos <kaio_matos22@hotmail.com>
2023-08-24 19:42:06 -0300

    amount reactivity improvement

--- src/models/Amount.ts
+++ src/models/Amount.ts
@@ -2,30 +2,48 @@ import { makeAutoObservable } from "mobx";
 import { Currency } from "./Currency";
 
 export class Amount {
-  currency: Currency;
-  value: number;
+  private _currency: Currency;
+  private _value: number;
 
   constructor(currency: Currency, value: number) {
-    this.currency = currency;
-    this.value = value;
+    this._currency = currency;
+    this._value = value;
     makeAutoObservable(this);
+  }
+
+  // Exposed variables via getters and setters for better reactivity
+  get value() {
+    return this._value;
+  }
+
+  set value(v: number) {
+    this._value = v;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(currency: Currency) {
+    this._currency = currency;
   }
 
   private get formatter() {
     try {
       return new Intl.NumberFormat(undefined, {
         style: "currency",
-        currency: this.currency.code,
+        currency: this._currency.code,
       });
     } catch {
       // fallback
       return {
-        format: (n: number) => `${this.currency.name} ${n}`,
+        format: (n: number) =>
+          `${this._currency.name} ${n.toFixed(this._currency.decimal_digits)}`,
       };
     }
   }
 
   get formatted() {
-    return this.formatter.format(this.value);
+    return this.formatter.format(this._value);
   }
 }
